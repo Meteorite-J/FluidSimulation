b5602e7367c6a84a677394388fe3e84a860b6e88
Jizhou Xu <mr.jizhou.xu@gmail.com>
2017-03-02 17:38:46 -0500

    Updated program resources.

--- js/liquid.simulator.js
+++ js/liquid.simulator.js
@@ -94,7 +94,7 @@ var Simulator = (function() {
         wgl.createProgramsFromFiles({
             transferToGridProgram: {
                 vertexShader: 'shaders/transfertogrid.vert',
-                fragmentShader: ['shaders/common.frag', 'shaders/transfertogrid.frag'],
+                fragmentShader: ['shaders/shared.frag', 'shaders/transfertogrid.frag'],
                 attributeLocations: {
                     'a_textureCoordinates': 0
                 }
@@ -115,14 +115,14 @@ var Simulator = (function() {
             },
             addForceProgram: {
                 vertexShader: 'shaders/fullscreen.vert',
-                fragmentShader: ['shaders/common.frag', 'shaders/addforce.frag'],
+                fragmentShader: ['shaders/shared.frag', 'shaders/addforce.frag'],
                 attributeLocations: {
                     'a_position': 0
                 }
             },
             enforceBoundariesProgram: {
                 vertexShader: 'shaders/fullscreen.vert',
-                fragmentShader: ['shaders/common.frag', 'shaders/enforceboundaries.frag'],
+                fragmentShader: ['shaders/shared.frag', 'shaders/enforceboundaries.frag'],
                 attributeLocations: {
                     'a_textureCoordinates': 0
                 }
@@ -136,42 +136,42 @@ var Simulator = (function() {
             },
             transferToParticlesProgram: {
                 vertexShader: 'shaders/fullscreen.vert',
-                fragmentShader: ['shaders/common.frag', 'shaders/transfertoparticles.frag'],
+                fragmentShader: ['shaders/shared.frag', 'shaders/transfertoparticles.frag'],
                 attributeLocations: {
                     'a_position': 0
                 }
             },
             divergenceProgram: {
                 vertexShader: 'shaders/fullscreen.vert',
-                fragmentShader: ['shaders/common.frag', 'shaders/divergence.frag'],
+                fragmentShader: ['shaders/shared.frag', 'shaders/divergence.frag'],
                 attributeLocations: {
                     'a_position': 0
                 }
             },
             jacobiProgram: {
                 vertexShader: 'shaders/fullscreen.vert',
-                fragmentShader: ['shaders/common.frag', 'shaders/jacobi.frag'],
+                fragmentShader: ['shaders/shared.frag', 'shaders/jacobi.frag'],
                 attributeLocations: {
                     'a_position': 0
                 }
             },
             subtractProgram: {
                 vertexShader: 'shaders/fullscreen.vert',
-                fragmentShader: ['shaders/common.frag', 'shaders/subtract.frag'],
+                fragmentShader: ['shaders/shared.frag', 'shaders/subtract.frag'],
                 attributeLocations: {
                     'a_position': 0
                 }
             },
             advectProgram: {
                 vertexShader: 'shaders/fullscreen.vert',
-                fragmentShader: ['shaders/common.frag', 'shaders/advect.frag'],
+                fragmentShader: ['shaders/shared.frag', 'shaders/advect.frag'],
                 attributeLocations: {
                     'a_position': 0
                 }
             },
             copyProgram: {
                 vertexShader: 'shaders/fullscreen.vert',
-                fragmentShader: 'shaders/copy.frag',
+                fragmentShader: 'shaders/duplicate.frag',
                 attributeLocations: {
                     'a_position': 0
                 }
